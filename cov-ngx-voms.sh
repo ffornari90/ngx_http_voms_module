362323feeca31a2c3692d134d4634c45dd72cd2b
Elisabetta Ronchieri <elisabetta.ronchieri@cnaf.infn.it>
2018-04-17 17:44:08 +0200

    Added options to change the build of html pages

--- cov-ngx-voms.sh
+++ cov-ngx-voms.sh
@@ -30,12 +30,11 @@
 # The script works best (i.e. it is tested) if run within a docker container
 # started from the storm2/ngx-voms-build image.
 
-mkdir -p cov
-cd cov
+#geninfo --base-directory ${HOME}/openresty-1.13.6.1/build/nginx-1.13.6/objs/addon/src/ --output-filename coverage.info ${HOME}/openresty-1.13.6.1/build/nginx-1.13.6/objs/addon/src/
+geninfo --output-filename /tmp/coverage.info ${HOME}/openresty-1.13.6.1/build/nginx-1.13.6/objs/addon/src/
+genhtml --prefix ${HOME}/openresty-1.13.6.1/build/nginx-1.13.6/objs/addon/src/ --ignore-errors source --demangle-cpp /tmp/coverage.info \
+  --legend --title "coverage ngix" --output-directory=/tmp/coverage-report
 
-geninfo --base-directory ${HOME}/openresty-1.13.6.1/build/nginx-1.13.6/objs/addon/src/ --output-filename coverage.info ${HOME}/openresty-1.13.6.1/build/nginx-1.13.6/objs/addon/src/
-
-genhtml --demangle-cpp coverage.info &>output
 exit_status=$?
 if [ ! $exit_status -eq 0 ]; then
    echo "check output"
