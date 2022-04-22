6114d45af5c9b91856bf7d3605a7841d8aa752a4
lcappelli <laura.cappelli@cnaf.infn.it>
2022-04-22 15:50:19 +0000

    Improve RUN layers
    * To reduce the image dimension, two RUN commands are merged in one single layer.
    * All packages are installed with a single command.
    * remove cern and garr mirrors
    * the clean commands are the last instructions in the layer
    * sudo is installed only in the dockerfile

--- .devcontainer/assets/provide-user.sh
+++ .devcontainer/assets/provide-user.sh
@@ -17,7 +17,6 @@ fi
 groupadd --gid $USER_GID $USERNAME
 useradd --uid $USER_UID --gid $USER_GID -m $USERNAME
 
-yum install -y sudo
 echo $USERNAME ALL=\(root\) NOPASSWD:ALL > /etc/sudoers.d/$USERNAME
 chmod 0440 /etc/sudoers.d/$USERNAME
 
