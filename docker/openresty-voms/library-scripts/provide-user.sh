d3f3a94052f023bdeb0f30fc6760c67991078cf7
lcappelli <laura.cappelli@cnaf.infn.it>
2022-07-01 10:53:00 +0000

    Update Dockerfile for rpm building
     * Use a *provide-user* script
     * Reduce RUN directives
     * Formatting
     * Transform ADD to COPY following the best practises

---
+++ docker/openresty-voms/library-scripts/provide-user.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Copyright 2018-2022 Istituto Nazionale di Fisica Nucleare
+# SPDX-License-Identifier: EUPL-1.2
+
+USERNAME=${1}
+USER_UID=${2}
+USER_GID=${3}
+
+set -ex
+
+groupadd --gid $USER_GID $USERNAME
+useradd --uid $USER_UID --gid $USER_GID -m $USERNAME
+
+echo $USERNAME ALL=\(root\) NOPASSWD:ALL > /etc/sudoers.d/$USERNAME
+chmod 0440 /etc/sudoers.d/$USERNAME
