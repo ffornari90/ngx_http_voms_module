7461f11081e29d929b0c2e6f46d6527fb145e245
lcappelli <laura.cappelli@cnaf.infn.it>
2022-09-23 14:23:06 +0000

    Re-add documentation comment

--- test-ngx-voms.sh
+++ test-ngx-voms.sh
@@ -3,6 +3,15 @@
 # Copyright 2018-2022 Istituto Nazionale di Fisica Nucleare
 # SPDX-License-Identifier: EUPL-1.2
 
+# The script requires the locations of the openresty bundle and of the
+# ngx_http_voms_module code (for example as checked-out from git). The locations
+# are expressed by the environment variables OPENRESTY_ROOT and
+# NGX_HTTP_VOMS_MODULE_ROOT respectively, if available. If they are not set,
+# they are guessed:
+# * a unique openresty bundle is looked for in ${HOME}
+# * the ngx_http_voms_module code is looked for in the working directory of the
+#   continuous integration environment first and then in ${HOME}
+
 if [ -r "${HOME}/openresty-env" ]; then
     . ${HOME}/openresty-env
 fi
